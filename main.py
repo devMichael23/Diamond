9c3df16dbbc93c5a080f523326e7a3d16a4acecd
Misha Petrushinin <petrushinin.mikhail@icloud.com>
2022-02-20 13:00:30 +0300

    Finally commit Task Close

--- main.py
+++ main.py
@@ -3,11 +3,32 @@ def get_string(string: str, level: int) -> str:
     return result
 
 
-number = int(input("Size of diamond: "))
-void_string = ""
-for i in range(number):
-    void_string += " "
+def delete_duplicates(ls: list[str], length: int):
+    for string in ls:
+        if string == '*' * length:
+            ls.remove(string)
+            break
 
-half_diamond = [void_string for _ in range(number)]
-for i in range(1, len(diamond)+1, 2):
-    half_diamond[i-1] = get_string(half_diamond[i-1], i)
+
+def clear_diamond(ls: list[str], length: int) -> list[str]:
+    result = []
+    for string in ls:
+        if string == ' ' * length:
+            continue
+        else:
+            result.append(string)
+    delete_duplicates(result, length)
+    return result
+
+
+number = int(input("Size of diamond: "))
+
+diamond = [' ' * number for _ in range(number)]
+for i in range(1, len(diamond) + 1, 2):
+    diamond[i - 1] = get_string(diamond[i - 1], i)
+for i in range(len(diamond), 0, -2):
+    diamond.append(get_string(diamond[i - 1], i))
+diamond = clear_diamond(diamond, number)
+
+for crystal in diamond:
+    print(crystal)
